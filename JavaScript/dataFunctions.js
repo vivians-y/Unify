19bfacc4b2ac2c608699a1b5cd3d9b4a0a838ba6
Pyristix <61607183+Pyristix@users.noreply.github.com>
2020-04-25 12:54:29 -0500

    Update dataFunctions.js

--- JavaScript/dataFunctions.js
+++ JavaScript/dataFunctions.js
@@ -1,33 +1,32 @@
-class dataFunctions{
+var userList = [];
+var taskList = [];
 
-	var userList = [];
-	var taskList = [];
-
+function getUserData(){
 	
-
-	function getUserData(){
-		
-	}
-	
-	function getTaskData(){
-		
-	}
-	
-	function filterTasks(filterType){ //Filter types are "VolunteeringOnly", "Food&WaterOnly", ""
-		
-		getTaskData(); //Refreshes tasks
-		
-		taskList.push(new Task("Help","Ted", "Volunteering", "I'm dying", 5, "My House"));
-		
-		for(i=0; i < taskList.length; i++){ //Removes tasks that don't fit criteria
-			if(filterType == ""){
-				
-			}
-			
-			
-		}
-		
-		
-	}
-
 }
+
+function getTaskData(){
+	
+}
+
+function filterTasks(filterType){ //Filter types are "VolunteeringOnly", "Food&WaterOnly", "LendingOnly", "UrgencyAtLeast2", "UrgencyAtLeast3", "UrgencyAtLeast4"
+	
+	getAllTasks(); //Refreshes tasks
+	
+	taskList.push(new Task("Help", "Ted", "Volunteering", "I'm dying", 5, "My House"));
+	
+	for(i=0; i < taskList.length; i++){ //Removes tasks that don't fit criteria
+		if(filterType == "VolunteeringOnly"){
+			console.log("Volunteering Only");
+		}
+
+		else if (filterType == "Food&WaterOnly"){
+			console.log("Food & Water Only");
+		}
+
+		else if (filterType == "LendingOnly"){
+			console.log("Lending Only");
+		}
+	}
+}
+
