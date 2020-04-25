279aa2c1325e1f12f63eb95cffd16f6682e27d77
Pyristix <61607183+Pyristix@users.noreply.github.com>
2020-04-25 02:33:57 -0500

    Task & User classes

--- JavaScript/user.js
+++ JavaScript/user.js
@@ -1,5 +1,23 @@
 class User{
+	var firstName;
+	var lastName;
+	var username;
+	var passw;
+	var karma;
+	
     constructor(){
-        this.userName = "";
+        this.firstName = "";
+		this.lastName = "";
+		this.username = "";
+		this.passw = "";
+		this.karma = 0;
+    }
+	
+	constructor(firstName,lastName,username,passw){
+        this.firstName = firstName;
+		this.lastName = lastName;
+		this.username = username;
+		this.passw = passw;
+		this.karma = 0;
     }
 }
