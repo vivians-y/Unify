8fbc54c6c7caa647b9c1a369dbdfe1f59afe4b91
Abhishek22002 <41200654+Abhishek22002@users.noreply.github.com>
2020-04-25 21:13:01 -0500

    E

--- www/HTML/maps.js
+++ www/HTML/maps.js
@@ -1,26 +1,58 @@
-
+let map, infoWindow;
 function initMap() {
     // map = new google.maps.Map(document.getElementById('map'), {
     //     center: {lat: 29.7604, lng: -95.3698},
     //     zoom: 7
     // });
-    var directionsService = new google.maps.DirectionsService();
-    var directionsRenderer = new google.maps.DirectionsRenderer();
-    showDist(29.7604,-95.3698,30.6280,-96.3344,directionsService,directionsRenderer)
-
+    // var directionsService = new google.maps.DirectionsService();
+    // var directionsRenderer = new google.maps.DirectionsRenderer();
+    //showDist(29.7604,-95.3698,30.6280,-96.3344,directionsService,directionsRenderer)
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -34.397, lng: 150.644},
+        zoom: 6
+    });
     let org1 = new google.maps.LatLng(29.7604,-95.3698);
     let dest1 = new google.maps.LatLng(30.6280,-96.3344);
     let originArr = [org1];
     let destinationArr = [dest1];
-    calcDist(originArr, destinationArr);
+    //calcDist(originArr, destinationArr);
+    infoWindow = new google.maps.InfoWindow;
+
+    // Try HTML5 geolocation.
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+        }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+        });
+    } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+    }
+
     var mapOptions = {
         zoom:7,
         center: {lat: 29.7604, lng: -95.3698}
     }
     //console.log("HI1132e4"+distances[0]);
-    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
-    directionsRenderer.setMap(map);
+    //var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    //directionsRenderer.setMap(map);
 
+}
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
 }
 function printDistances(distances){
     console.log(distances.length);
