1e949ca4c1088cbd3acca9af146a84e09ea82e69
Abhishek22002 <41200654+Abhishek22002@users.noreply.github.com>
2020-04-26 15:12:14 -0500

    E

--- www/JavaScript/profileUpdater.js
+++ www/JavaScript/profileUpdater.js
@@ -19,7 +19,7 @@ if (currUser.phoneNumber != null && currUser.phoneNumber.length > 0) {
 }
 // location
 if (currUser.latitude != null && currUser.longitude != null && currUser.latitude != -1 && currUser.longitude != -1) {
-    document.getElementById("profileLocation").innerText = `Location: (${currUser.latitude}, ${currUser.longitude})`;
+    document.getElementById("profileLocation").innerText = `Location: (${currUser.latitude.toFixed(3)}, ${currUser.longitude.toFixed(3)})`;
 } else {
     document.getElementById("profileLocation").innerText = `Location: (N/A, N/A)`;
 }
@@ -54,8 +54,8 @@ function giveTextBoxesShit() {
     }
 // location
     if (currUser.latitude != null && currUser.longitude != null && currUser.latitude != -1 && currUser.longitude != -1) {
-        document.getElementById("profileLatitudeEdit").placeholder = `Latitude: ${currUser.latitude}`;
-        document.getElementById("profileLongitudeEdit").placeholder = `Longitude ${currUser.longitude})`;
+        document.getElementById("profileLatitudeEdit").placeholder = `Latitude: ${currUser.latitude.toFixed(3)}`;
+        document.getElementById("profileLongitudeEdit").placeholder = `Longitude ${currUser.longitude.toFixed(3)})`;
     } else {
         document.getElementById("profileLatitudeEdit").placeholder = `Latitude: N/A`;
         document.getElementById("profileLongitudeEdit").placeholder = `Longitude: N/A`;
@@ -71,26 +71,26 @@ function editFields() {
     let newLatitude = document.getElementById("profileLatitudeEdit").value;
     let newLongitude = document.getElementById("profileLongitudeEdit").value;
     let user = JSON.parse(sessionStorage.getItem("currentUser"));
-    if(newFirstName != null && newFirstName.length > 0){
+    if (newFirstName != null && newFirstName.length > 0) {
         user.firstName = newFirstName;
     }
-    if(newLastName != null && newLastName.length > 0){
+    if (newLastName != null && newLastName.length > 0) {
         user.lastName = newLastName;
     }
-    if(newEmail != null && newEmail.length > 0){
+    if (newEmail != null && newEmail.length > 0) {
         user.email = newEmail;
     }
-    if(newPhone != null && newPhone.length > 0){
+    if (newPhone != null && newPhone.length > 0) {
         user.phoneNumber = newPhone;
     }
-    if(newLatitude != null && newLatitude.length > 0 && !isNaN(newLatitude)){
+    if (newLatitude != null && newLatitude.length > 0 && !isNaN(newLatitude)) {
         user.latitude = parseFloat(newLatitude);
     }
-    if(newLongitude != null && newLongitude.length > 0 && !isNaN(newLongitude)){
+    if (newLongitude != null && newLongitude.length > 0 && !isNaN(newLongitude)) {
         user.longitude = parseFloat(newLongitude);
     }
     // username down here because you need to check if username is being used
-    if(newUsername != null && newUsername.length > 0){
+    if (newUsername != null && newUsername.length > 0) {
         console.log("checking old users");
         // check old usernames
         getAllUsers(function (oldUsers) {
@@ -118,7 +118,7 @@ function editFields() {
                 // update all the tasks with new username
                 getAllTasks(function (allTasks) {
                     for (let i = 0; i < allTasks.length; i++) {
-                        if(allTasks[i].taskCreatorUsername == oldUsername){
+                        if (allTasks[i].taskCreatorUsername == oldUsername) {
                             allTasks[i].taskCreatorUsername = newUsername;
 
                             updateTask(allTasks[i], function () {
@@ -134,8 +134,7 @@ function editFields() {
             // anything you want to do after the save here
             updateFields();
         });
-    }
-    else {
+    } else {
         // don't need to change username
         console.log("username else");
         updateUser(user, function () {
@@ -176,8 +175,29 @@ function updateFields() {
     }
 // location
     if (currUser.latitude != null && currUser.longitude != null && currUser.latitude != -1 && currUser.longitude != -1) {
-        document.getElementById("profileLocation").innerText = `Location: (${currUser.latitude}, ${currUser.longitude})`;
+        document.getElementById("profileLocation").innerText = `Location: (${currUser.latitude.toFixed(3)}, ${currUser.longitude.toFixed(3)})`;
     } else {
         document.getElementById("profileLocation").innerText = `Location: (N/A, N/A)`;
     }
+}
+
+function updateLoc() {
+    getCurrPos(callBackCoolBoy);
+}
+
+function callBackCoolBoy(valout) {
+
+    let user1 = JSON.parse(sessionStorage.getItem("currentUser"));
+
+// to change
+    user1.latitude = valout.lat;
+    user1.longitude = valout.lng;
+//varName."other field (user.js)" = newVal;
+
+// to save
+    updateUser(user1, function () {
+        sessionStorage.setItem("currentUser", JSON.stringify(user1));
+        // anything you want to do after the save here
+        updateFields();
+    });
 }
